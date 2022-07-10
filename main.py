37df97836d02c43605e6031f715c7cead593c32a
Shrek <100253910+Shrek069@users.noreply.github.com>
2022-07-10 09:41:15 +0530

    Create main.py
    main file

---
+++ main.py
@@ -0,0 +1,33 @@
+# Asks for sum
+
+n = int(input("First Number: "))
+
+s =input("Symbol: ")
+
+n2 = int(input("Second Number: "))
+
+# ----------------------------------------------------
+
+# Division
+
+if s == "/":
+
+	 print(n, "/", n2, "=", n / n2)	 
+
+# Addition 
+
+if s == "+":
+
+	 print(n, "+", n2, "=", n + n2 )
+
+# Subtraction 
+
+if s == "-":
+
+	 print(n, "-", n2, "=", n - n2)
+
+# Multiplication 
+
+if s == "*":
+
+	 print(n, "*", n2, "=", n * n2)
